af7d853d726c994d2bae3cc1bf96dab1e5cee0ef
Ahn-Ssu <21600402@handong.edu>
2021-03-23 17:30:33 +0900

    solved p2

---
+++ hw2/hw2_2.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    DIR *dp;
+    struct dirent *dent;
+    char wd[BUFSIZ];
+
+    if (( dp = opendir(".")) == NULL){
+        perror("opendir : './'");
+        exit(1);
+    }
+    
+    while (( dent = readdir(dp))){
+        printf("%s ", dent->d_name);
+        if (dent->d_type == 4){
+            printf("[directory]\n");
+        }
+        else printf("\n");
+    }
+    
+
+    return 0;
+}
