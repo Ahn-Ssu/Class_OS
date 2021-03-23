23cecd7cbaac81e799878447a72f0d505822fe5e
Ahn-Ssu <21600402@handong.edu>
2021-03-23 16:32:56 +0900

    나름 디테일 챙기고 hw2_1 풀었ㅇ므

--- hw2/hw2_1.c
+++ hw2/hw2_1.c
@@ -10,96 +10,110 @@
 #define MAX_CMD 2048
 #define MAX_ARG 256
 
-
-
-
 void ParseCommand(char *command, int *argc, char *argv[]);
-int main(){
-	char command[MAX_CMD];
-	command[0] = command[MAX_CMD-1] = 0 ; 
-  DIR *dp;
-  struct dirent *dent;
-  char *cwd;
-  char wd[BUFSIZ];
+int main()
+{
+    char command[MAX_CMD];
+    command[0] = command[MAX_CMD - 1] = 0;
+    DIR *dp;
+    struct dirent *dent;
+    char *cwd;
+    char wd[BUFSIZ];
 
-	int argc = 0 ;
-	char *argv[MAX_ARG] = {NULL};
+    int argc = 0;
+    char *argv[MAX_ARG] = {NULL};
 
-	while(1){
-		printf("$ ");
-		fgets(command, MAX_CMD-1,stdin);
-		command[strlen(command) -1] = 0 ;
+    while (1)
+    {
+        printf("$ ");
+        fgets(command, MAX_CMD - 1, stdin);
+        command[strlen(command) - 1] = 0;
 
-		if( strcmp(command, "quit") == 0 || strcmp (command, "exit") == 0)
-			break;
+        if (strcmp(command, "quit") == 0 || strcmp(command, "exit") == 0)
+            break;
 
-    if(!strcmp(command,"")) continue; // 아무것도 입력하지 않았을 때
+        if (!strcmp(command, ""))
+            continue; // 아무것도 입력하지 않았을 때
 
-		ParseCommand(command, &argc, argv);
-    int result = 0;
-		  if (!strcmp(argv[0],"ls")){
-    system("ls");
-	}
-  else if (!strcmp(argv[0], "cd")){
-    
-    if (argv[1]==NULL){
-      chdir("~");
+        ParseCommand(command, &argc, argv);
+        int result = 0;
+        if (!strcmp(argv[0], "ls"))
+        {
+            system("ls");
+        }
+        else if (!strcmp(argv[0], "cd"))
+        {
+
+            if (argv[1] == NULL) // cd 만 입력했을때 홈 디렉토리로 가주는 것
+            {
+                chdir(getenv("HOME")) 
+            }
+            else if (!strcmp(argv[1],"~")){
+                chdir(getenv("HOME"));
+            }
+            else if (!strcmp(argv[1],"~/")){
+                chdir(getenv("HOME"));
+            }
+            else
+            {
+                chdir(argv[1]);
+            }
+        }
+        else if (!strcmp(argv[0], "mkdir"))
+        {
+            if (argv[1] == NULL)
+            {
+                printf("usage: mkdir [directory name]\n"); // 명령어만 입력하고 만들 디렉토리를 안 줬을때
+            }
+            else if (mkdir(argv[1], 0755) == -1)
+            {
+                perror(argv[1]);
+            }
+        }
+        else if (!strcmp(argv[0], "rmdir"))
+        {
+            if (argv[1] == NULL)
+            {
+                printf("usage: rmdir [directory name]\n"); // 명령어만 입력하고 만들 디렉토리를 안 줬을때
+            }
+            else if (rmdir(argv[1]) == -1)
+            {
+                perror(argv[1]);
+            }
+        }
+
+        else if (!strcmp(argv[0], "curdir"))
+        {
+
+            cwd = getcwd(NULL, BUFSIZ);
+            printf("current directory = %s\n", cwd);
+        }
+        else
+        {
+            printf("%s : command not found\n", argv[0]);
+        }
     }
-    else{
-      chdir(argv[1]);
-    }
-  }
-  else if (!strcmp(argv[0], "mkdir")){
-    if(argv[1]==NULL){
-      printf("usage: mkdir [directory name]\n"); // 명령어만 입력하고 만들 디렉토리를 안 줬을때
-    }
-    else if(mkdir(argv[1], 0755)== -1){
-      perror(argv[1]);
-    }
-  }
-  else if (!strcmp(argv[0], "rmdir")){
-    if(argv[1]==NULL){
-      printf("usage: rmdir [directory name]\n"); // 명령어만 입력하고 만들 디렉토리를 안 줬을때
-    }
-    else if(rmdir(argv[1])==-1){
-      perror(argv[1]);
-    }
-    
-  }
-  
-  else if (!strcmp(argv[0], "curdir")){
 
-    cwd = getcwd(NULL, BUFSIZ);
-    printf("current directory = %s\n", cwd);
-  }
-  else{
-    printf("%s : command not found\n",argv[0]);
-  }
-  }
-	
+    printf("Bye!\n");
 
-	printf("Bye!\n");
-
-	return 0;
+    return 0;
 }
 
+void ParseCommand(char *command, int *argc, char *argv[])
+{
 
+    for (int i = 0; i < MAX_ARG; i++)
+        argv[i] = 0;
 
+    char *p = strtok(command, " ");
+    int count = 0;
 
-void ParseCommand(char *command, int *argc, char *argv[]){
+    while (p != NULL)
+    {
+        argv[count] = p;
 
-	for(int i = 0 ; i < MAX_ARG ; i++)
-		argv[i] = 0 ;
-
-	char *p = strtok(command, " ");
-	int count=0;
-
-	while(p != NULL){
-		argv[count] = p;
-
-		p = strtok(NULL, " ");
-		count++;
-	}
-	*argc = count;
+        p = strtok(NULL, " ");
+        count++;
+    }
+    *argc = count;
 }
-
