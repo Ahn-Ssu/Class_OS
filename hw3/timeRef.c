f8219025592bbd9b02fb46354556e6e12aea21da
Ahn-Ssu <21600402@handong.edu>
2021-04-07 17:30:16 +0900

    from blog

---
+++ hw3/timeRef.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <time.h> 
+
+int main() { 
+  struct tm *date;
+  const time_t t = time(NULL);
+  
+  date = localtime(&t);
+  printf( "%d/%d/%d %d:%d:%d\n" ,
+   date->tm_year + 1900 ,
+   date->tm_mon + 1 , 
+   date->tm_mday , 
+   date->tm_hour , 
+   date->tm_min , 
+   date->tm_sec ); 
+   
+   return 0;
+    }
+
