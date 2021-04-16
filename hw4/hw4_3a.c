33cc15a1bae92479525dbf0d7fc724142e7dc249
Ahn-Ssu <21600402@handong.edu>
2021-04-16 23:25:26 +0900

    일단 inst. 대로 수행함

--- hw4/hw4_3a.c
+++ hw4/hw4_3a.c
@@ -11,45 +11,65 @@
 
 #define SHM_FILE "key_buffer.shm"
 
-
 int main(int argc, char *argv[])
 {
 	// TO DO: open SHM_FILE for using shm_open()
-	//	check if the file was successfully open
+
+	int shm_fd;
+	const char *name = "OS_hw4";
+
+	shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+	if (shm_fd == -1)
+		errExit("shm_open"); //	check if the file was successfully open
 
 	int buffer_size = sizeof(KeyBuffer);
 
 	// TO DO: set size of the shared memory by buffer_size
+	if (ftruncate(shm_fd, buffer_size) == -1)
+		errExit("ftruncate");
 
 	KeyBuffer *key_buffer = NULL;
 	// TO DO: map the shared memory file and receive the return address into key_buffer
 	//	check if the file was successfully mapped
+	struct shmbuf *shared_memory = mmap(0, buffer_size, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+	if (shared_memory == MAP_FAILED)
+		errExit("mmap");
 
 	// TO DO: initialize the key buffer in the shared memory
+	InitKeyBuffer(key_buffer);
 
 	clrscr();
 
 	printf("i: up, j: left, k: down, l: right, c: change color, q: quit hw4_3b, ESC: quit both\n");
 
-	while(1){
+	while (1)
+	{
 		gotoxy(1, 2);
 		printf("Input a key: ");
 		int key = getch();
 
-		/* TO DO: implement the following action
-			if key is ESC (ASCII 27), insert 'q' into the key buffer and break the loop
-			otherwise, insert the key into the key buffer.
-
-		*/
-
+		if (key == 27)
+		{
+			InsertKey(key_buffer, 'q');
+			break;
+		}
+		else
+		{
+			InsertKey(key_buffer, key);
+		}
 		DisplayKeyBuffer(1, 2, key_buffer);
 	}
 
 	// TO DO: unmap and unlink the shared memory block
+
+	if (munmap(shm_fd, buffer_size) == -1)
+		errExit("munmap");
+
+	if (shm_unlink(name) == -1)
+		errExit("shm_unlink");
 
 	clrscr();
 	printf("Bye!\n");
 
 	return 0;
 }
-
